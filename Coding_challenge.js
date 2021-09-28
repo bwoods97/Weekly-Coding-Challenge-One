d311ca06494fb6720829067c574371cb9d666617
bwoods97 <57374238+bwoods97@users.noreply.github.com>
2021-09-28 10:09:37 -0400

    Add files via upload

---
+++ Coding_challenge.js
@@ -0,0 +1,21 @@
+const arrayIndex = {
+    indOne: 1,
+    indTwo: 10,
+    indThree: 100,
+    indFour: 1000,
+    indFive: 10000,
+    indSix: 1000000,
+
+};
+
+const keyView = Object.keys(arrayIndex);
+
+console.log(keyView);
+
+const valueView = Object.values(arrayIndex);
+
+console.log(valueView);
+
+const objectView = Object.entries(arrayIndex);
+
+console.log(objectView);
